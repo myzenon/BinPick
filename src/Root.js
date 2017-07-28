9687fb283e51ab221b1e56263bc97ca80ade73b9
Iammiind <Onanong.Pureemahawong@gmail.com>
2017-07-29 00:58:52 +0700

    Create Category Component
    create UI of the component with mockup data.

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+<<<<<<< HEAD
 import Navigation from './Navigation'
+=======
+import { View } from 'react-native'
+import Category from './components/Category/index.js'
+>>>>>>> f2428acb499bd7fa683bf0308ee984c94d5b3376
 
 export default class Root extends Component {
-    state = {  }
     render() {
         return (
+<<<<<<< HEAD
             <Navigation />
+=======
+            <Category />
+>>>>>>> f2428acb499bd7fa683bf0308ee984c94d5b3376
         )
     }
 }
