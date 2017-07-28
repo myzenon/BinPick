ed9e78d07c30c9f78145d70ac7f01ba217382add
Zenon 'SI <si.zenon@gmail.com>
2017-07-28 15:14:54 +0700

    Init Commit

---
+++ src/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class Root extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
