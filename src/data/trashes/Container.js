a12ef7c4f5a5494aa23d5d568eae4df3aac0a7d9
DESKTOP-QKKUBVT\pariy <pariyathida@gmail.com>
2017-07-31 22:08:50 +0700

    add @1x, @2x of container

--- src/data/trashes/Container.js
+++ src/data/trashes/Container.js
@@ -32,7 +32,7 @@ export default Container = {
                 en: 'can',
                 th: 'กระป๋อง'
             },
-            image: require('../../images/trashes/container/product/can/can.png'),
+            image: require('../../images/trashes/container/product/can/can.jpg'),
             bins: [
                 'recycle',
                 'general',
@@ -118,7 +118,7 @@ export default Container = {
                 en: 'plastic container',
                 th: 'บรรจุภัณฑ์พลาสติก'
             },
-            image: require('../../images/trashes/container/product/plasticContainer/plasticContainer.png'),
+            image: require('../../images/trashes/container/product/plasticContainer/plasticContainer.jpg'),
             bins: [
                 'recycle',
                 'general',
@@ -131,7 +131,7 @@ export default Container = {
                         en: 'The plastic container should be empty.',
                         th: 'บรรจุภัณฑ์ที่จะทิ้งควรเป็นบรรจุภัณฑ์ที่ว่างเปล่า'
                     },
-                    image: require('../../images/trashes/container/product/plasticContainer/tip/1/emptyContainer.png')
+                    image: require('../../images/trashes/container/product/plasticContainer/tip/1/emptyContainer.jpg')
                 }
              ]
         },
