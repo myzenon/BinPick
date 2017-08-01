316bd1c55b490534634211d06a63167dd3daff8b
Iammiind <Onanong.Pureemahawong@gmail.com>
2017-08-01 16:53:30 +0700

    Update BinDetails Component

--- src/locales/en.js
+++ src/locales/en.js
@@ -7,5 +7,7 @@ export default {
     newTrashButton: 'SELECT NEW WASTE CATEGORY',
     tip: 'Tips',
     binTip: 'This trash should be in the',
-    lastWordColorBin: 'bin'
+    lastWordColorBin: 'bin',
+    binDetails: 'Bin Information',
+    Description: 'Description'
 }
